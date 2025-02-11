a398584ca8842bed17e4aab67fef389526dbc169
Xiangyu Chen <chenxy327@gmail.com>
2025-02-11 03:49:25 -0500

    misc changes related to ShiftController and non-alignment

--- Objects/TileForTilemapShiftController.gd
+++ Objects/TileForTilemapShiftController.gd
@@ -1,10 +1,10 @@
 #when shifting, tile should decelerate upon bounce and accelerate otherwise
-#upon bounce, snap to nearest cell, even if heading in original shift dir (not reversed)
 class_name TileForTilemapShiftController
 extends TileForTilemapController;
 
 var max_speed:float;
 var src_pos_t:Vector2i;
+var curr_pos_t:Vector2i;
 var dir:Vector2i;
 var remaining_dist:float;
 
@@ -12,23 +12,29 @@ var remaining_dist:float;
 func _init(tile:TileForTilemap, pos_t:Vector2i, dir:Vector2i, target_dist_t:int):
 	self.tile = tile;
 	src_pos_t = pos_t;
+	curr_pos_t = pos_t;
 	self.dir = dir;
 	remaining_dist = GV.TILE_WIDTH * target_dist_t;
 	max_speed = target_dist_t * GV.TILE_WIDTH * GV.SHIFT_DISTANCE_TO_MAX_SPEED;
 
-#update velocity (decelerate if bounce else accelerate)
 func step(delta:float):
 	#update position
 	var prev_position:Vector2 = tile.position;
 	tile.velocity = tile.velocity.lerp(max_speed * dir, GV.SHIFT_LERP_WEIGHT);
-	tile.move_and_collide(tile.velocity * delta);
+	var collision:KinematicCollision2D = tile.move_and_collide(tile.velocity * delta);
 	
 	#update remaining_dist
 	var true_step_dist:float = Vector2(dir).dot(tile.position - prev_position);
 	remaining_dist -= true_step_dist;
+	
+	#bounce (with deceleration), update curr_pos_t and tilemap if tile exited last cell
+	if remaining_dist <= GV.SNAP_TOLERANCE:
+		tile.world.set_atlas_coords(GV.LayerId.TILE, )
+	else:
+		
 
 #returns true if bounce succeeds (not blocked by sliding tile)
-#when bouncing, snap to nearest cell
+#after reverse, snap to nearest cell, even if heading in original shift dir (not reversed)
 func reverse():
 	reversed = not reversed;
 	dir *= -1;
