30c13464638d8ab44fabe31c70a66deff2ea1cf8
Xiangyu Chen <chenxy327@gmail.com>
2025-02-17 00:00:26 -0500

    fix duang anim not completing

--- Objects/TileForTilemapSprite.gd
+++ Objects/TileForTilemapSprite.gd
@@ -5,28 +5,27 @@ extends Sprite2D
 signal freed;
 var tile:TileForTilemap;
 var animators:Dictionary; #animator_type -> animator
+var governor_tile:TileForTilemap;
 
 
 func _init(tile:TileForTilemap, tile_sheet:CompressedTexture2D, tile_atlas_coords:Vector2i, z_index:int, conversion_anim_ids:Array[int], governor_tile:TileForTilemap):
 	self.tile = tile;
 	self.z_index = z_index;
-	
+	self.governor_tile = governor_tile;
+	for anim_id in conversion_anim_ids:
+		add_animator(anim_id, governor_tile);
+
 	#set texture
 	hframes = GV.TILE_SHEET_HFRAMES;
 	vframes = GV.TILE_SHEET_VFRAMES;
 	frame_coords = tile_atlas_coords;
 	texture = tile_sheet;
-	
-	#add animators
-	for anim_id in conversion_anim_ids:
-		add_animator(anim_id, governor_tile);
 
 func _physics_process(delta:float) -> void:
 	for key in animators.keys():
 		var animator:TileForTilemapSpriteAnimator = animators[key];
 		
 		if not animator.step(delta):
-			animator.queue_free();
 			animators.erase(key);
 
 func add_animator(conversion_anim_id:int, governor_tile:TileForTilemap):
