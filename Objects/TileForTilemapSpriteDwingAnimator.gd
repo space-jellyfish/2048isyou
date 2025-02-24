b55f42338712c4141b75c606b37aaf7267349dd0
Xiangyu Chen <chenxy327@gmail.com>
2025-02-24 00:26:34 -0500

    add important note in finalize_transit() description; also debug prints

--- Objects/TileForTilemapSpriteDwingAnimator.gd
+++ Objects/TileForTilemapSpriteDwingAnimator.gd
@@ -6,10 +6,13 @@ var angle_rad:float = GV.DWING_START_ANGLE;
 
 #should sync to slide progress in case tile bounces
 func step(delta:float):
+	if is_governor_tile_finished:
+		print();
 	#progress scale animation
 	angle_rad += anim_dir * GV.DWING_SPEED;
 	angle_rad = clamp(angle_rad, GV.DWING_START_ANGLE, GV.DWING_END_ANGLE);
 	sprite.scale = GV.DWING_FACTOR / sin(angle_rad) * Vector2.ONE;
+	#print("dwing angle: ", angle_rad);
 	
 	if is_governor_tile_finished and \
 		((angle_rad == GV.DWING_END_ANGLE and not is_reversed) or \
