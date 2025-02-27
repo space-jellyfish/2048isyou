23ee94df0a005ddc6bcec51caf79f9bb9ddef2b8
Xiangyu Chen <chenxy327@gmail.com>
2025-02-27 06:22:15 -0500

    fix typo and nav refcount not set properly

--- Objects/TileForTilemapSpriteDwingAnimator.gd
+++ Objects/TileForTilemapSpriteDwingAnimator.gd
@@ -6,8 +6,6 @@ var angle_rad:float = GV.DWING_START_ANGLE;
 
 #should sync to slide progress in case tile bounces
 func step(delta:float):
-	if is_governor_tile_finished:
-		print();
 	#progress scale animation
 	angle_rad += anim_dir * GV.DWING_SPEED;
 	angle_rad = clamp(angle_rad, GV.DWING_START_ANGLE, GV.DWING_END_ANGLE);
