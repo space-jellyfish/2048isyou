eb16561b7d591e4b46d70388216c91f56636ffea
Xiangyu Chen <chenxy327@gmail.com>
2025-03-01 02:27:08 -0500

    fix tile being set on finalize_transit if is_merging and is_splitted

--- Objects/TileForTilemap.gd
+++ Objects/TileForTilemap.gd
@@ -386,8 +386,9 @@ func finalize_transit(prev_transit_id:int, is_aligned:bool, pos_t:Vector2i, is_r
 	# update tilemap TILE layer
 	var is_poolable:bool = is_aligned and conversion_transit_id == GV.TransitId.NONE and move_transit_id == GV.TransitId.NONE;
 	if is_poolable:
-		if ((prev_transit_id == GV.TransitId.SPLIT or is_splitted) and not is_reversed) or prev_transit_id == GV.TransitId.MERGE or ((is_merging or is_splitted) and is_reversed) or (prev_transit_id in [GV.TransitId.SLIDE, GV.TransitId.SHIFT] and not is_splitted and not is_merging):
+		if (prev_transit_id == GV.TransitId.SPLIT and not is_reversed) or (is_merging and is_reversed) or (not is_merging and prev_transit_id != GV.TransitId.SPLIT):
 			var final_atlas_coords:Vector2i = world.get_doubled_tile_atlas_coords(atlas_coords) if is_splitted and is_reversed else atlas_coords;
+			print("set ", pos_t, final_atlas_coords);
 			world.set_atlas_coords(GV.LayerId.TILE, pos_t, GV.TileSetSourceId.TILE, final_atlas_coords);
 			
 	# update tilemap NAV layer
@@ -411,6 +412,7 @@ func finalize_transit(prev_transit_id:int, is_aligned:bool, pos_t:Vector2i, is_r
 		else:
 			if is_merging:
 				world.add_tile_in_transient(merger_tile);
+				print(world.get_atlas_coords(GV.LayerId.TILE, merger_tile.pos_t, false))
 				assert(world.get_atlas_coords(GV.LayerId.TILE, merger_tile.pos_t, false) == -Vector2i.ONE);
 				world.set_atlas_coords(GV.LayerId.TILE, merger_tile.pos_t, GV.TileSetSourceId.TILE, merger_tile.atlas_coords, 1, false);
 			if is_splitted:
