c143daea6cd2a3fc3d3c20e242eea89c828caa78
Xiangyu Chen <chenxy327@gmail.com>
2025-03-05 04:08:22 -0500

    misc GDE stuff

--- GDExtension/duplicator/duplicator_path_controller.cpp
+++ GDExtension/duplicator/duplicator_path_controller.cpp
@@ -1,6 +1,7 @@
 #include <godot_cpp/variant/vector2i.hpp>
 #include <godot_cpp/classes/ref_counted.hpp>
 #include <godot_cpp/classes/mutex.hpp>
+#include "actions.h"
 #include "duplicator_path_controller.h"
 
 using namespace std;
@@ -12,6 +13,8 @@ void DuplicatorPathController::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("get_gv"), &DuplicatorPathController::get_gv);
 	ClassDB::bind_method(D_METHOD("set_world", "w"), &DuplicatorPathController::set_world);
 	ClassDB::bind_method(D_METHOD("get_world"), &DuplicatorPathController::get_world);
+    ClassDB::bind_method(D_METHOD("get_danger_lv"), &DuplicatorPathController::get_danger_lv);
+    ClassDB::bind_method(D_METHOD("get_danger_escape_dir"), &DuplicatorPathController::get_danger_escape_dir);
 
     ADD_PROPERTY(PropertyInfo(Variant::OBJECT, "gv", PROPERTY_HINT_NODE_TYPE, "Node"), "set_gv", "get_gv");
 	ADD_PROPERTY(PropertyInfo(Variant::OBJECT, "world", PROPERTY_HINT_NODE_TYPE, "Node2D"), "set_world", "get_world");
@@ -33,33 +36,79 @@ Node2D* DuplicatorPathController::get_world() {
     return world;
 }
 
-// updates danger_lv and danger_escape_dir
+void DuplicatorPathController::set_cells(TileMap* t) {
+    cells = t;
+}
+
+TileMap* DuplicatorPathController::get_cells() {
+    return cells;
+}
+
+
+uint8_t DuplicatorPathController::get_tile_id(Vector2i pos_t) {
+    Vector2i atlas_coords = cells->get_cell_atlas_coords(LayerId::TILE, pos_t);
+    return atlas_coords_to_tile_id(atlas_coords);
+}
+
+uint8_t DuplicatorPathController::get_type_id(Vector2i pos_t) {
+    Vector2i atlas_coords = cells->get_cell_atlas_coords(LayerId::TILE, pos_t);
+    return atlas_coords_to_type_id(atlas_coords);
+}
+
+uint8_t DuplicatorPathController::get_back_id(Vector2i pos_t) {
+    Vector2i atlas_coords = cells->get_cell_atlas_coords(LayerId::BACK, pos_t);
+    return atlas_coords_to_back_id(atlas_coords);
+}
+
+uint16_t DuplicatorPathController::get_nav_id(Vector2i pos_t) {
+    Vector2i atlas_coords = cells->get_cell_atlas_coords(LayerId::NAV, pos_t);
+    return atlas_coords_to_nav_id(atlas_coords);
+}
+
+uint32_t DuplicatorPathController::get_stuff_id(Vector2i pos_t) {
+    return make_tile_bits(get_tile_id(pos_t)) + make_type_bits(get_type_id(pos_t)) + make_back_bits(get_back_id(pos_t)) + make_nav_bits(get_nav_id(pos_t));
+}
+
+
+// neighbor entry (dir : Danger) not added if neighbor isn't a duplicator
+void DuplicatorPathController::get_world_info(Vector2i pos_t, Vector2i min_pos_t, vector<vector<uint32_t>>& lv, unordered_map<Vector2i, Danger>& neighbors) {
+    Array layer_mutexes = world->get("layer_mutexes");
+    Ref<Mutex> tile_mutex = layer_mutexes[LayerId::TILE];
+    // ================ START CRITICAL SECTION ================
+    tile_mutex->lock();
+
+    for (int dx = 0; dx < lv[0].size(); ++dx) {
+        Vector2i curr_pos_t = Vector2i(min_pos_t.x + dx, pos_t.y);
+        Vector2i curr_lv_pos = curr_pos_t - min_pos_t;
+        lv[curr_lv_pos.y][curr_lv_pos.x] = get_stuff_id(curr_pos_t);
+    }
+    for (int dy = 0; dy <= lv.size(); ++dy) {
+        Vector2i curr_pos_t = Vector2i(pos_t.x, min_pos_t.y + dy);
+        Vector2i curr_lv_pos = curr_pos_t - min_pos_t;
+        lv[curr_lv_pos.y][curr_lv_pos.x] = get_stuff_id(curr_pos_t);
+    }
+    for (auto& [dir_id, dir] : DIRECTIONS) {
+        Vector2i curr_pos_t = pos_t + dir;
+        uint8_t curr_type_id = get_type_id(curr_pos_t);
+        if (curr_type_id == TypeId::DUPLICATOR) {
+            Ref<RefCounted> curr_tile_entity = world->call("get_aligned_tile_entity", curr_type_id, curr_pos_t);
+            DuplicatorPathController* path_controller = RefCounted::cast_to<DuplicatorPathController>(curr_tile_entity->get("path_controller"));
+            neighbors[dir] = path_controller->danger;
+        }
+    }
+
+    tile_mutex->unlock();
+    // ================ END CRITICAL SECTION ================
+}
+
 // check all four neighbors for tiles with higher merge priority, or other duplicators from which danger can be inherited
 // danger_lv is set to DANGER_LV_MAX if adjacent to a higher-merge-priority tile, or neighbor.danger_lv - 1 if adjacent to another duplicator
 // danger_escape_dir is set to point away from souce of highest danger_lv, or any one if there are multiple
 // NOTE assumes each entity has unique merge_priority
 void DuplicatorPathController::update_danger(Vector2i pos_t) {
-    Dictionary type_enum = gv->get("TypeId");
-    int type_id = type_enum["DUPLICATOR"];
-    Dictionary merge_priorities = gv->get("merge_priorities");
-    int merge_priority = merge_priorities[type_id];
-    Array layer_mutexes = world->get("layer_mutexes");
-    int tile_layer_id = static_cast<Dictionary>(gv->get("LayerId"))["TILE"];
-    Array directions = static_cast<Dictionary>(gv->get("DIRECTIONS")).values();
-
-    danger_lv = 0;
-
-    for (int i = 0; i < directions.size(); ++i) {
-        Vector2i dir = directions[i];
+    for (auto& [dir_id, dir] : DIRECTIONS) {
         Vector2i curr_pos_t = pos_t + dir;
-        // ================ START CRITICAL SECTION ================
-        Ref<Mutex> tile_mutex = layer_mutexes[tile_layer_id];
-        tile_mutex->lock();
-        int curr_type_id = world->call("get_type_id", curr_pos_t, false);
-        Ref<RefCounted> curr_tile_entity = world->call("get_aligned_tile_entity", curr_type_id, curr_pos_t);
-        tile_mutex->unlock();
-        // ================ END CRITICAL SECTION ================
-        int curr_merge_priority = merge_priorities[curr_type_id];
+        int slide_push_count = get_slide_push_count(lv, )
 
         if (curr_merge_priority > merge_priority) {
             danger_lv = DANGER_LV_MAX;
@@ -80,6 +129,13 @@ void DuplicatorPathController::update_danger(Vector2i pos_t) {
 
 // action dir should be normalized
 Vector3i DuplicatorPathController::get_action(Vector2i pos_t) {
+    // get world info
+    Vector2i min_pos_t = Vector2i(pos_t.x - LV_RADIUS, pos_t.y - LV_RADIUS);
+    vector<vector<uint32_t>> lv = vector<vector<uint32_t>>(LV_WIDTH, vector<uint32_t>(LV_WIDTH, StuffId::NONE));
+    unordered_map<Vector2i, Danger> neighbors;
+    get_world_info(pos_t, min_pos_t, lv, neighbors);
+
+    // update danger
     update_danger(pos_t);
 
     // escape has highest priority
--- GDExtension/duplicator/duplicator_path_controller.h
+++ GDExtension/duplicator/duplicator_path_controller.h
@@ -13,7 +13,8 @@
 #include <godot_cpp/classes/ref_counted.hpp>
 #include <godot_cpp/classes/node.hpp>
 #include <godot_cpp/classes/node2d.hpp>
-#include "OpenClosedList.h"
+#include <godot_cpp/classes/tile_map.hpp>
+#include "actions.h"
 
 using namespace std;
 using namespace godot;
@@ -24,13 +25,20 @@ class DuplicatorPathController : public RefCounted {
     GDCLASS(DuplicatorPathController, RefCounted);
 
 private:
+    struct Danger {
+        int level = 0;
+        Vector2i escape_dir = DIRECTIONS.at(DirectionId::RIGHT);
+    };
+
     static const int DANGER_LV_MAX = 2;
+    int LV_RADIUS = tile_push_limits.at(EntityId::DUPLICATOR) + 1;
+    int LV_WIDTH = 2 * LV_RADIUS + 1;
 
     Node* gv = nullptr;
     Node2D* world = nullptr;
+    TileMap* cells = nullptr;
 
-    int danger_lv = 0;
-    Vector2i danger_escape_dir = Vector2i(1, 0);
+    Danger danger;
 
 protected:
 	static void _bind_methods();
@@ -40,7 +48,19 @@ public:
     Node* get_gv();
     void set_world(Node2D* w);
     Node2D* get_world();
+    void set_cells(TileMap* t);
+    TileMap* get_cells();
 
+    int get_danger_lv();
+    Vector2i get_danger_escape_dir();
+
+    uint8_t get_tile_id(Vector2i pos_t);
+    uint8_t get_type_id(Vector2i pos_t);
+    uint8_t get_back_id(Vector2i pos_t);
+    uint16_t get_nav_id(Vector2i pos_t);
+    uint32_t get_stuff_id(Vector2i pos_t);
+
+    void get_world_info(Vector2i pos_t, Vector2i min_pos_t, vector<vector<uint32_t>>& lv, unordered_map<Vector2i, Danger>& neighbors);
     void update_danger(Vector2i pos_t);
     Vector3i get_action(Vector2i pos_t);
 };
