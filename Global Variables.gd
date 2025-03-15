c40d745b7822c90573d38c4d7fa731e8f1b955a1
Xiangyu Chen <chenxy327@gmail.com>
2025-03-15 00:36:36 -0400

    reset duplicator stats in GV

--- Global Variables.gd
+++ Global Variables.gd
@@ -610,7 +610,7 @@ const UNINITIATED_PREMOVE_COOLDOWN_DISCOUNT:float = 0.5;
 var action_cooldowns:Dictionary = {
 	EntityId.NONE : 0,
 	EntityId.PLAYER : 0,
-	EntityId.DUPLICATOR : 4, #14,
+	EntityId.DUPLICATOR : 12,
 	EntityId.HOSTILE : 0.8,
 	EntityId.VOID : 0.5,
 	EntityId.REGULAR : 0,
@@ -625,7 +625,7 @@ var action_cooldowns:Dictionary = {
 var action_cooldown_deviations:Dictionary = {
 	EntityId.NONE : 0,
 	EntityId.PLAYER : 0,
-	EntityId.DUPLICATOR : 0.5, #3,
+	EntityId.DUPLICATOR : 2.5,
 	EntityId.HOSTILE : 0,
 	EntityId.VOID : 0,
 	EntityId.REGULAR : 0,
@@ -639,7 +639,7 @@ var action_cooldown_deviations:Dictionary = {
 var base_spawn_weights:Dictionary = {
 	TypeId.NONE : 0,
 	TypeId.PLAYER : 0,
-	TypeId.DUPLICATOR : 2000,#4,
+	TypeId.DUPLICATOR : 4,
 	TypeId.HOSTILE : 50,
 	TypeId.VOID : 2,
 	TypeId.REGULAR : 10000,
